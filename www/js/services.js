db74ac237887d310333360c14d43a8bd0ddeca8c
Alexandru Matei <alexandru.matei@busymachines.com>
2014-10-18 14:07:19 +0300

    Added also the .html and .js files

---
+++ www/js/services.js
@@ -0,0 +1,50 @@
+angular.module('activityApp.services',[]).factory('Activity',['$http','PARSE_CREDENTIALS',function($http,PARSE_CREDENTIALS){
+    return {
+        getAll:function(){
+            return $http.get('https://api.parse.com/1/classes/Activity',{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                }
+            });
+        },
+        get:function(id){
+            return $http.get('https://api.parse.com/1/classes/Activity/'+id,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                }
+            });
+        },
+        create:function(data){
+            return $http.post('https://api.parse.com/1/classes/Activity',data,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        },
+        edit:function(id,data){
+            return $http.put('https://api.parse.com/1/classes/Activity/'+id,data,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        },
+        delete:function(id){
+            return $http.delete('https://api.parse.com/1/classes/Activity/'+id,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        }
+    }
+}]).value('PARSE_CREDENTIALS',{
+    APP_ID: 'eC1j5NAJbb8vutRDIQpMg1SMWYLiex7L9FvjSGtU',
+    REST_API_KEY:'lJt3r37CrpAQBeJF7PoGrH2hNIoHSLDbLyutsxPX'
+});
